ce9b28aedfdd42ac306d40f852687b3e328e7fb1
maratustra <maratustraa@gmail.com>
2021-07-13 18:30:39 +0500

    create Yandex map API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+// Создание интеркативной карты Яндекс API
+ymaps.ready(init);
+
+let myMap;
+let myPlacemark;
+
+function init() {
+
+  myMap = new ymaps.Map("map", {
+    center: [7.573638, 79.803867], // Координаты центра карты
+    zoom: 10  // Уровень масштабирования от 0 (весь мир) до 19.
+  }, {
+    searchControlProvider: 'yandex#search'
+  })
+
+  myPlacemark = new ymaps.GeoObject({ // Создаем геообъект с типом геометрии "Точка"
+
+    geometry: {
+      type: "Point",
+      coordinates: [7.577567, 79.794274]
+    }
+  })
+}
+
+
 const swiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
