4c36acf4712fdff66e61fffd37a2441ce9ac5d26
maratustra <maratustraa@gmail.com>
2021-07-15 13:08:48 +0500

    fix: fixed mobile width for slider & got back google maps

--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
-// Создание интеркативной карты Яндекс API
-ymaps.ready(init);
+// // Создание интеркативной карты Яндекс API
+// ymaps.ready(init);
 
-function init() {
+// function init() {
 
-  var myMap = new ymaps.Map("map", {
+//   var myMap = new ymaps.Map("map", {
 
-    center: [7.573638, 79.803867], // Координаты центра карты
-    zoom: 10  // Уровень масштабирования от 0 (весь мир) до 19.
-  }, {
-    searchControlProvider: 'yandex#search'
+//     center: [7.573638, 79.803867], // Координаты центра карты
+//     zoom: 10  // Уровень масштабирования от 0 (весь мир) до 19.
+//   }, {
+//     searchControlProvider: 'yandex#search'
 
-  })
+//   })
 
-  myGeoObject = new ymaps.GeoObject({
+//   myGeoObject = new ymaps.GeoObject({
 
-    geometry: {
-      type: "Point",
-      coordinates: [55.8, 37.8]
-    },
+//     geometry: {
+//       type: "Point",
+//       coordinates: [55.8, 37.8]
+//     },
 
-  });
+//   });
 
-  myMap.geoObjects
-    .add(new ymaps.Placemark([7.577567, 79.794274], {
-      balloonContent: 'Маленькая иконка'
-    }, {
-      iconLayout: 'default#image'
-    }))
-}
+//   myMap.geoObjects
+//     .add(new ymaps.Placemark([7.577567, 79.794274], {
+//       balloonContent: 'Маленькая иконка'
+//     }, {
+//       iconLayout: 'default#image'
+//     }))
+// }
 
 
-const swiper = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+// const swiper = new Swiper('.hotel-slider', {
+//   // Optional parameters
+//   loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.hotel-slider__button--next',
+//     prevEl: '.hotel-slider__button--prev',
+//   },
 
-  effect: 'coverflow',
-  coverflowEffect: {
-    rotate: 30,
-    slideShadows: false,
-  },
+//   effect: 'coverflow',
+//   coverflowEffect: {
+//     rotate: 30,
+//     slideShadows: false,
+//   },
 
-  // Keyboard Control Parameters
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+//   // Keyboard Control Parameters
+//   keyboard: {
+//     enabled: true,
+//     onlyInViewport: false,
+//   },
+// });
